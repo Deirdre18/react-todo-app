bbb7a910bb4a16da91c9d55a9297bc024ea27ffe
Deirdre18 <deirdreweldon18@gmail.com>
2020-06-24 16:16:49 +0100

    added inline styling to <div> in TodoItem.js

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,7 +5,8 @@ import PropTypes from 'prop-types';
 export class TodoItem extends Component {
     render() {
         return (
-            <div>
+            //in JSX we can actually use inline style which allows us to have our markup our, our functionality and our styling all within our component, so lets add styling in <div> and we use double {} for inline styling. There is no hyphenatio in inline styling and camelCase is used
+            <div style= {{ backgroundColor: '#9999ff' }}>
                 <p>{ this.props.todo.title }</p>
             </div>
         )
@@ -13,7 +14,7 @@ export class TodoItem extends Component {
 }//PropTypes
   //we want to define any prop-types for this class
 
-  //so name of class (Todos) and then it's going to be a single object of Prop, and is required. This is good practice to do
+  //so name of class (Todos) and then a single object of Prop, and is required. This is good practice to do
   TodoItem.propTypes = { 
     todo: PropTypes.object.isRequired
     }

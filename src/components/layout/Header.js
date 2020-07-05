c24d21a3281c091d1e629ea85b6f14ebbc5a73a7
Deirdre18 <deirdreweldon18@gmail.com>
2020-07-06 00:02:52 +0100

    added <Link style> and const linkStyle to App.js

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,12 +3,14 @@
 // functional component is one return, which works like render does
 
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
         //will be some links for about page but won't do that just yet
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link style={ linkStyle } to="/">Home</Link> | <Link style={ linkStyle } to="/">About</Link>
         </header>
         )
 }
@@ -22,6 +24,11 @@ const headerStyle={
     textAlign: 'center',
     padding: '10px'
     
+}
+
+const linkStyle = {
+    color: '#fff',
+    textDecoration: 'none'
 }
 
 export default Header;
